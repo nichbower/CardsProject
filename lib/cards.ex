90347db82e0173d93f222134f68a61fb75c21439
nichbower <nickb3310@gmail.com>
2019-10-21 22:40:41 -0400

    Initial commit

---
+++ lib/cards.ex
@@ -0,0 +1,18 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello()
+      :world
+
+  """
+  def create_deck do
+    ["Ace", "Two", "Three", "One"]
+  end
+end
